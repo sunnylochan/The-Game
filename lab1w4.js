66cb909c1eb2bea4eb93bf32fb6eec4335d0d483
Sunny <99064548@mydavinci.nl>
2020-09-27 23:54:59 +0200

    kaas code

---
+++ lab1w4.js
@@ -0,0 +1,69 @@
+var geelk = prompt("Is de kaas geel?");
+
+if (geel == "ja"){
+	alert("De kaas is geel");
+	var gaten = prompt("Zitten er gaten in de kaas?");}
+		else{
+			alert("De kaas is niet geel");
+			var schimmels = prompt("Heeft de kaas blauwe schimmels?");
+		}
+		if (schimmels == "ja"){
+			alert("Er zitten geen schimmels op de kaas");
+			var korst = prompt("Heeft de kaas een korst?");
+				else{
+					var korstt = prompt("Heeft de kaas een korst?");}
+				
+					if (korstt == "Ja"){
+					alert("Camembert");
+				}
+				else{
+					alert("Mozzarella");
+				}
+
+			}
+			if (korst == "ja"){
+				alert("Bleu de Rochbaron");
+			}
+			else{
+				alert("Foume d'Ambert")
+			}
+			if (gaten == "ja"){
+				alert("Er zitten gaten in de kaas");
+				var duur = prompt("Is de kaas belachelijk duur?");}
+			else{
+				alert("Er zitten gaten in de kaas");
+				var hard = prompt("Is de kaas zo hard als steen?");
+				if (hard == "ja"){
+					alert("Pammigiano Reggiano");
+				}
+				else{
+					alert("Goude kaas");
+				}
+			}	
+			if ( duur == "ja"){
+				alert("Emmenthaler")
+			}
+				else{
+					alert("Leerdammer");
+				}
+
+			
+		
+
+
+
+
+
+
+
+
+				
+
+
+
+
+		
+		
+		
+	
+ 
